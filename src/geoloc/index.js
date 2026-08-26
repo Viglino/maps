8e584a73f94d6c548cc72e7e6464bf2bb5e961a4
viglino <jean-marc.viglino@ign.fr>
2026-08-26 15:14:16 +0200

    [ADD] validate response

--- src/geoloc/index.js
+++ src/geoloc/index.js
@@ -1,3 +1,4 @@
+import { getDistance, toKMString } from './utils.js'
 import "./index.css"
 
 let mapAPI;
@@ -6,6 +7,17 @@ const game = {};
 MapIFrameAPI.ready('map', function(api) {
   window.mapAPI = mapAPI = api;
   mapAPI.setLayer({ id: 2, displayInLayerSwitcher: false, visible: false });
+  mapAPI.setLayer({ id: 1, visible: true });
+  mapAPI.setCenter([6.937, 48.297]);
+  mapAPI.setZoom(12);
+  mapAPI.on('move', e => {
+    console.log(e)
+    if (e.zoom > 15) {
+      document.body.dataset.validate = 'true';
+    } else {
+      delete document.body.dataset.validate;
+    }
+  });
   mapAPI.getFeatures({ layerId: 2 }, features => {
     game.features = features;
     // ready
@@ -14,7 +26,41 @@ MapIFrameAPI.ready('map', function(api) {
 });
 
 
+document.querySelector('main aside button.ortho').addEventListener('click', e => {
+  mapAPI.setLayer({ id: 4, visible: true });
+})
+document.querySelector('main aside button.map').addEventListener('click', e => {
+  mapAPI.setLayer({ id: 4, visible: false });
+})
+
+
+document.querySelector('.validate button').addEventListener('click', e => {
+  mapAPI.getCenter(pos => {
+    if (game.feature) {
+      const dist = getDistance(pos, game.feature.geometry.coordinates);
+      if (dist < 1000) {
+        ddist.dataset.win = '';
+      } else {
+        delete ddist.dataset.win;
+      }
+      ddist.dataset.dist = dist;
+      ddist.querySelector('span').textContent = toKMString(dist);
+      ddist.showModal();
+    };
+  });
+});
+
 const dlog = document.body.querySelector('dialog.start');
+const ddist = document.body.querySelector('dialog.dist');
+ddist.querySelector('button.again').addEventListener('click', e => {
+  ddist.close();
+})
+ddist.querySelector('button.win').addEventListener('click', e => {
+  ddist.close();
+  getImage();
+})
+
+
 const zoomDiv = document.querySelector('.zoom');
 const img = document.querySelector('aside img');
 
@@ -24,11 +70,11 @@ function zoom(b) {
     zoom = document.querySelector(".zoom").clientWidth;
   }
   zoom += b * 200;
-  if (img.clientHeight < img.clientWidth) {
-    zoom = Math.max(zoom, document.querySelector(".zoom").clientWidth * img.clientHeight / img.clientWidth);
+  if (img.naturalHeight < img.naturalWidth) {
+    zoom = Math.max(zoom, document.querySelector(".zoom").clientWidth * img.naturalHeight / img.naturalWidth);
     zoomDiv.style.backgroundSize = `auto ${zoom}px`;
   } else {
-    zoom = Math.max(zoom, document.querySelector(".zoom").clientWidth * img.clientWidth / img.clientHeight);
+    zoom = Math.max(zoom, document.querySelector(".zoom").clientWidth * img.naturalWidth / img.naturalHeight);
     zoomDiv.style.backgroundSize = `${zoom}px auto`;
   }
   document.body.dataset.img = zoom;
---
+++ src/geoloc/utils.js
@@ -0,0 +1,135 @@
+/** Get the extent of a geometry
+ * @param {Object} geom - The geometry object (GeoJSON format)
+ * @returns {Array} extent - An array containing the extent [minX, minY, maxX, maxY]
+ */
+function getExtent(geom) {
+  if (geom.type === 'Polygon') {
+    geom = geom.coordinates[0];
+  } else if (geom.type === 'MultiPolygon') {
+    geom = geom.coordinates[0][0];
+  } else if (geom.type === 'Point') {
+    geom = [geom.coordinates];
+  } else if (geom.type === 'LineString') {
+    geom = geom.coordinates;
+  }
+  const extent = [+Infinity, +Infinity, -Infinity, -Infinity];
+  geom.forEach(coord => {
+    if (coord[0] < extent[0] || !extent[0]) extent[0] = coord[0];
+    if (coord[1] < extent[1] || !extent[1]) extent[1] = coord[1];
+    if (coord[0] > extent[2] || !extent[2]) extent[2] = coord[0];
+    if (coord[1] > extent[3] || !extent[3]) extent[3] = coord[1];
+  });
+  return extent
+}
+
+/** Get the center of a geometry
+ * @param {Object} geom - The geometry object (GeoJSON format)
+ * @returns {Array} center - An array containing the center [longitude, latitude]
+ */
+function getCenter(geom) {
+  const extent = getExtent(geom);
+  return [(extent[0] + extent[2]) /2, (extent[1] + extent[3]) /2];
+}
+
+/** Get the distance between two coordinates
+ * This uses the Haversine formula to calculate the distance between two points on the Earth
+ * It assumes the Earth is a perfect sphere, which is a good approximation for small distances.
+ * @see https://en.wikipedia.org/wiki/Haversine_formula
+ * @param {Array} coord1 - First coordinate [longitude, latitude]
+ * @param {Array} coord2 - Second coordinate [longitude, latitude]
+ * @return {number} distance - Distance in meters
+ */
+function getDistance(coord1, coord2) {
+  const toRad = deg => deg * Math.PI / 180;
+  const R = 6371e3; // Rayon de la Terre en mètres
+  const [lon1, lat1] = coord1;
+  const [lon2, lat2] = coord2;
+  const phi1 = toRad(lat1);
+  const phi2 = toRad(lat2);
+  const deltaPhi = toRad(lat2 - lat1);
+  const deltaLambda = toRad(lon2 - lon1);
+  const a = Math.sin(deltaPhi/2) * Math.sin(deltaPhi/2) +
+            Math.cos(phi1) * Math.cos(phi2) *
+            Math.sin(deltaLambda/2) * Math.sin(deltaLambda/2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  return Math.round(R * c);
+}
+
+/** Get km string
+ * @param {number} dist - Distance in meters
+ */
+function toKMString(dist, dec) {
+  if (dec === undefined) dec = 2;
+  // Convert distance to kilometers
+  if (dist > 10000) {
+    return (dist / 1000).toLocaleString(undefined, { maximumFractionDigits: dec}) + ' km';
+  } else {
+    return dist.toLocaleString() + ' m';
+  }
+}
+
+function getNearest(pt, features) {
+  let d = Infinity;
+  let pos = 0;
+  features.forEach((f, i) => {
+    const di = getDistance(pt, getCenter(f.geometry))
+    if (di < d) {
+      pos = i;
+      d = di;
+    }
+  })
+  return pos
+}
+
+/** Convert coords to degre
+ */
+function convertDDToDMS(D, lng) {
+  const dir = D < 0 ? (lng ? "O" : "S") : lng ? "E" : "N"
+  const deg = 0 | (D < 0 ? (D = -D) : D)
+  const min = 0 | (((D += 1e-9) % 1) * 60)
+  const sec = (0 | (((D * 60) % 1) * 6000)) / 100
+  return deg + '° ' + (min < 10 ?  '0':'') + min + '\' ' + (sec<10 ? '0':'') + sec.toFixed(2) + '" ' + dir
+}
+
+
+function getDMS(pt) {
+  const lon = convertDDToDMS(pt[0], true)
+  const lat = convertDDToDMS(pt[1])
+  return lon + '<br/>' + lat
+}
+
+function getHMS(t, short) {
+  t /= 1000
+  const mn = Math.trunc(t/60)
+  const s = t - mn*60
+  if (short) {
+    return mn + '\'' + (s<10 ? '0':'') + s.toFixed(0) +'"'
+  } else {
+    return mn + ' mn ' + (s<10 ? '0':'') + s.toFixed(0) +' s'
+  }
+}
+
+
+
+function createElement(tag, options) {
+  const elt = document.createElement(tag.toLowerCase())
+  Object.keys(options).forEach(k => {
+    switch(k) {
+      case 'parent': {
+        options.parent.appendChild(elt)
+        break;
+      }
+      case 'html': {
+        elt.innerHTML = options.html
+        break;
+      }
+      default: {
+        elt[k] = options[k]
+        break;
+      }
+    }
+  })
+  return elt;
+}
+
+export { getExtent, getCenter, getDistance, toKMString, getNearest, getDMS, getHMS, createElement };
